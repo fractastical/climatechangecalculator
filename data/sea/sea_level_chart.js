bdaf86647e2fffcc7c40081356340a805d4667d5
Joel Dietz <jdietz+github@gmail.com>
2023-10-24 04:51:02 +0200

    add rate calculator

--- data/sea/sea_level_chart.js
+++ data/sea/sea_level_chart.js
@@ -72,3 +72,42 @@ function decimalYearToDate(decimalYear) {
 
     return new Date(start.getTime() + remainder);
 }
+
+
+// Function to convert decimal year to a Date object
+function decimalYearToDate(decimalYear) {
+    var year = parseInt(decimalYear);
+    var remainder = (decimalYear - year) * (365 * 24 * 60 * 60 * 1000);
+    var start = new Date(year, 0, 1);
+
+    return new Date(start.getTime() + remainder);
+}
+
+// Current date and date a decade ago
+var now = new Date();
+var decadeAgo = new Date();
+decadeAgo.setFullYear(now.getFullYear() - 10);
+
+// Filter data from the last decade
+var recentData = seaLevelData.filter(function(record) {
+    var recordDate = decimalYearToDate(record.year);
+    return recordDate >= decadeAgo;
+});
+
+// Ensure there's enough data to calculate the rate
+if (recentData.length < 2) {
+    console.log("Not enough data");
+} else {
+    // Get the earliest and latest sea level records
+    var earliestRecord = recentData[0];
+    var latestRecord = recentData[recentData.length - 1];
+
+    // Calculate sea level change and time elapsed
+    var seaLevelChange = latestRecord.gmsl_variation - earliestRecord.gmsl_variation;
+    var timeElapsed = latestRecord.year - earliestRecord.year; // in years, since the data uses decimal years
+
+    // Calculate the average rate of change
+    var averageRateOfChange = seaLevelChange / timeElapsed; // in mm per year
+
+    console.log("The average rate of sea level change over the last decade is " + averageRateOfChange.toFixed(2) + " mm per year.");
+}
